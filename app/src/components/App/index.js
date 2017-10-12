d006d4d189d817163a28ba462bcfd4e426ce31b1
Maxime Vasse <mallaxoz@gmail.com>
2017-10-12 10:21:26 +0200

    modele react

---
+++ app/src/components/App/index.js
@@ -0,0 +1,25 @@
+/*
+ * Npm import
+ */
+import React from 'react';
+
+
+/*
+ * Local import
+ */
+
+
+/*
+ * Code
+ */
+const App = () => (
+  <div id="app">
+    <div id="app-hello">Hello World!</div>
+  </div>
+);
+
+
+/*
+ * Export default
+ */
+export default App;
